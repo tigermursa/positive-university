60d7a896448dbb83639bf669b865d30d97c7c844
Mursalin Hossain <mursalin2311@gmail.com>
2023-12-29 20:12:14 +0600

    "created admin"

---
+++ src/app/modules/Admin/admin.controller.ts
@@ -0,0 +1,61 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import { AdminServices } from './admin.services';
+import sendResponse from '../../utils/sendResponds';
+
+
+
+const getSingleAdmin = catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const result = await AdminServices.getSingleAdminFromDB(id);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Admin is retrieved succesfully',
+        data: result,
+    });
+});
+
+const getAllAdmins = catchAsync(async (req, res) => {
+    const result = await AdminServices.getAllAdminsFromDB(req.query);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Admins are retrieved succesfully',
+        data: result,
+    });
+});
+
+const updateAdmin = catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const { admin } = req.body;
+    const result = await AdminServices.updateAdminIntoDB(id, admin);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Admin is updated succesfully',
+        data: result,
+    });
+});
+
+const deleteAdmin = catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const result = await AdminServices.deleteAdminFromDB(id);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Admin is deleted succesfully',
+        data: result,
+    });
+});
+
+export const AdminControllers = {
+    getAllAdmins,
+    getSingleAdmin,
+    deleteAdmin,
+    updateAdmin,
+};
